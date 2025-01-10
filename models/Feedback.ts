cc1138d02f8dce8ed9ebe6b0ce91a3a11e63b23c
Kameer Hosein <kameer-hosien@outlook.com>
2025-01-10 10:54:31 -0400

    added created at date for feedback schema

--- models/Feedback.ts
+++ models/Feedback.ts
@@ -3,7 +3,9 @@ import mongoose, { Schema } from "mongoose";
 const feedbackSchema=new Schema({
     firstName:String,
     lastName:String,
-    feedback:String
+    feedback:String,
+    createdAt: { type: Date, default: Date.now },
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' },
 })
 
 const Report=mongoose.model("Report",feedbackSchema)
